9d685acac22eaa1a4b5554bb77d42a1b6893bc19
Akira KAWATA <a.kawashiro@gmail.com>
2018-03-27 13:06:37 +0900

    Left let letrec match-all in applyImplConv'

--- hs-src/Language/Egison/ImplConv.hs
+++ hs-src/Language/Egison/ImplConv.hs
@@ -1,6 +1,6 @@
 {- |
 Module      : Language.Egison.Types
-Copyright   : Akira Kawata
+Copyright   : Akira KAWATA
 Licence     : MIT
 
 This module contains implicit conversion algorithm for Egison
@@ -13,8 +13,9 @@ module Language.Egison.ImplConv(
   )where
 
 import qualified Language.Egison.Expressions as EE
-import Language.Egison.Expressions (Type(..), TypeVarIndex, EgisonExpr(..), EgisonTopExpr(..), Env, refEnvImplConv)
-import Control.Monad.Reader (Reader, ask, runReader)
+import Language.Egison.Expressions (Type(..), EgisonExpr(..), EgisonTopExpr(..), Env, refEnvImplConv, deleteEnvType)
+import Language.Egison.Types (innersToExprs)
+import Control.Monad.Reader (Reader, ask, local, runReader)
 
 implConvTopExpr :: Env -> EgisonTopExpr -> [EgisonExpr]
 implConvTopExpr env exp = case exp of
@@ -29,6 +30,42 @@ applyImplConv' e@(CharExpr _) = applyImplConv'' e TypeChar
 applyImplConv' e@(StringExpr _) = applyImplConv'' e TypeString
 applyImplConv' e@(BoolExpr _) = applyImplConv'' e TypeBool
 applyImplConv' e@(IntegerExpr _) = applyImplConv'' e TypeInt
+-- I must modify here after implement type environment.
+applyImplConv' e@(VarExpr _) = return [e]
+applyImplConv' (IfExpr e1 e2 e3) = do
+  e1s <- applyImplConv' e1
+  e2s <- applyImplConv' e2
+  e3s <- applyImplConv' e3
+  return $ IfExpr <$> e1s <*> e2s <*> e3s
+applyImplConv' (TupleExpr es) = do
+  ess <- mapM applyImplConv' es
+  return $ TupleExpr <$> ess
+applyImplConv' (CollectionExpr es) = do
+  let es1 = innersToExprs es
+  ess <- mapM applyImplConv' es1
+  return $ TupleExpr <$> (cartesian ess)
+applyImplConv' (LambdaExpr args body) = do
+    let args1 = filter (/= EE.Var []) $ map f args
+    -- To realize shadowing of variables in args, we must delete args from environment
+    body1 <- local (\e -> deleteEnvType e args1) $ applyImplConv' body
+    return $ map (\x -> LambdaExpr args x) body1
+      where f (EE.TensorArg s) = EE.Var [s]
+            f _ = EE.Var []
+applyImplConv' (ApplyExpr f e) = do
+  f1 <- applyImplConv' f
+  e1 <- applyImplConv' e
+  return $ ApplyExpr <$> f1 <*> e1
+
+
+-- This helper function take a list of list and return all Cartesian product.
+-- For example, lltol [[1,2],[3,4]] ===>> [[1,3],[1,4],[2,3],[2,4]]
+cartesian :: [[a]] -> [[a]]
+cartesian [] = [[]]
+cartesian (l:rest) = do
+  a <- l
+  as <- cartesian rest
+  return (a:as)
+
 
 applyImplConv'' :: EgisonExpr -> Type -> Reader Env [EgisonExpr]
 applyImplConv'' e t = do
