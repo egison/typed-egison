5a2d2c3847f0e6daa3eb43ccd52bb3446bf9b012
Akira KAWATA <a.kawashiro@gmail.com>
2018-03-27 15:38:27 +0900

    The left part of ImplConv is variable.

--- hs-src/Language/Egison/ImplConv.hs
+++ hs-src/Language/Egison/ImplConv.hs
@@ -14,7 +14,7 @@ module Language.Egison.ImplConv(
 
 import qualified Language.Egison.Expressions as EE
 import Language.Egison.Expressions (Type(..), EgisonExpr(..), EgisonTopExpr(..), Env, refEnvImplConv, deleteEnvType)
-import Language.Egison.Types (innersToExprs)
+import Language.Egison.Types (innersToExprs, removeTensorMap)
 import Control.Monad.Reader (Reader, ask, local, runReader)
 
 implConvTopExpr :: Env -> EgisonTopExpr -> [EgisonExpr]
@@ -47,7 +47,7 @@ applyImplConv' (CollectionExpr es) = do
 applyImplConv' (LambdaExpr args body) = do
     let args1 = filter (/= EE.Var []) $ map f args
     -- To realize shadowing of variables in args, we must delete args from environment
-    body1 <- local (\e -> deleteEnvType e args1) $ applyImplConv' body
+    body1 <- local (\e -> deleteEnvType e args1) $ applyImplConv' (removeTensorMap body)
     return $ map (\x -> LambdaExpr args x) body1
       where f (EE.TensorArg s) = EE.Var [s]
             f _ = EE.Var []
@@ -58,11 +58,28 @@ applyImplConv' (ApplyExpr f e) = do
 applyImplConv' (LetExpr binds exp) = do
   -- head is appended because the bug of let binds
   let vars = map (head.fst) binds
+  let bodies = map snd binds
+  bodies1 <- mapM applyImplConv' bodies
   exp1 <- local (\e -> deleteEnvType e vars) $ applyImplConv' exp
-  return undefined
+  return $ do
+    let bodies2 = cartesian bodies1
+    bodies3 <- bodies2
+    exp2 <- exp1
+    return $ LetExpr (zip (map fst binds) bodies3) exp2
+applyImplConv' (LetRecExpr binds exp) = do
+  es <- applyImplConv' (LetExpr binds exp)
+  return $ map (\(LetExpr b e) -> LetRecExpr b e) es
+applyImplConv' (MatchAllExpr e1 e2 (pt,e3)) = do
+  e11 <- applyImplConv' e1
+  e21 <- applyImplConv' e2
+  e31 <- applyImplConv' e3
+  return $ MatchAllExpr <$> e11 <*>  e21 <*> (zip (inf pt) e31)
+    where
+      inf x = x : inf x
+applyImplConv' e@_ = return [e]
 
 -- This helper function take a list of list and return all Cartesian product.
--- For example, lltol [[1,2],[3,4]] ===>> [[1,3],[1,4],[2,3],[2,4]]
+-- For example, cartesian [[1,2],[3,4]] ===>> [[1,3],[1,4],[2,3],[2,4]]
 cartesian :: [[a]] -> [[a]]
 cartesian [] = [[]]
 cartesian (l:rest) = do
